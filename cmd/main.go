5cc7e50331f55a25c3713972e3c85576585f6579
blacmarllbor0 <3100194@gmail.com>
2023-01-23 19:08:04 +0300

    adding DI

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	restgo "github.com/go-rest-api"
 	"github.com/go-rest-api/pkg/handler"
+	"github.com/go-rest-api/pkg/repository"
+	"github.com/go-rest-api/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
+
 	server := new(restgo.Server)
 	if err := server.Run("8080", handlers.InitRouter()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
---
+++ pkg/service/service.go
@@ -0,0 +1,19 @@
+package service
+
+import "github.com/go-rest-api/pkg/repository"
+
+type Authorization interface{}
+
+type TodoList interface{}
+
+type TodoItem interface{}
+
+type Service struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewService(repo *repository.Repository) *Service {
+	return &Service{}
+}
